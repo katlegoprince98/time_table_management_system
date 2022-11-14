50fe8947f86004f416b33a599dec157258a05ed6
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2022-11-14 20:28:29 +0200

    ff

--- forms/course.php
+++ forms/course.php
@@ -18,7 +18,7 @@
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Course code</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="text" class="form-control" id="exampleInputPassword1">
   </div>
   
   <button type="submit" class="btn btn-primary">Submit</button>
